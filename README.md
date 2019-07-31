cdf0215fa1bed3e4f03321a0b52d1a0acb5d406b
Nick Makarov <nmakarov@gmail.com>
2019-07-31 15:29:50 -0700

    adding datasource: adjectives

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # lists
 Lists API server and React app tutorial
+
+
+## Steps
+
+### Step 1
+Get'em started
+something that works - countries list
+adding filter and count
+
+### Step 2
+More data - adjectives
+more filters
+how long does it take to generate the response?
+
+
+## Data sources
+
+### Countries
+https://gist.github.com/keeguon/2310008
+https://github.com/samayo/country-json
+https://github.com/icyrockcom/country-capitals/blob/master/data/country-list-with-ids.json
+
+### Adjectives
+https://www.enchantedlearning.com/wordlist/adjectives.shtml
+https://patternbasedwriting.com/elementary_writing_success/list-4800-adjectives/
+
+
--- api/server.js
+++ api/server.js
@@ -8,12 +8,26 @@ app.get("/version", (req, res) => {
 app.get("/countries", (req, res) => {
     let data = require("./data/countries");
     const { startsWith } = req.query;
-    console.info(">> startsWith:", startsWith);
 
     if (startsWith) {
         data = data.filter($ => (new RegExp(`^${startsWith}`, "i")).test($.name));
     }
+
     data = data.map($ => $.name);
+    res.json({
+        data,
+        length: data.length
+    });
+});
+
+app.get("/adjectives", (req, res) => {
+    let data = require("./data/adjectives").adjectives;
+    const { startsWith } = req.query;
+
+    if (startsWith) {
+        data = data.filter($ => (new RegExp(`^${startsWith}`, "i")).test($));
+    }
+
     res.json({
         data,
         length: data.length
